54cfca2666b02ea2aba8cd45be11366f811588b2
Nilesh <nilesh.jaiswar006@gmail.com>
2025-03-28 02:54:07 +0530

    implemented auth routes setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes.js'
+
+const app = express()
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.send("Hello Nilesh")
+})
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
