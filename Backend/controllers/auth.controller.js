1dbe960ca85c7f50997537825fe61b90948d0843
Nilesh <nilesh.jaiswar006@gmail.com>
2025-03-28 22:16:56 +0530

    generated jwt and implemented email and jwt in controller

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,18 +1,31 @@
 import User from "../models/user.model.js"
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
-    const {fullName, username, password, confirmPassword, gender} = req.body
+    const {fullName, username, email, password, confirmPassword, gender} = req.body
+
+    if (!email) {
+      return res.status(400).json({ error: "Email is required" });
+    }
 
     if (password !== confirmPassword){
       return res.status(400).json({error: "Passwords do not match"})
     }
 
-    const user = await User.findOne({username})
+    const user = await User.findOne({username, email})
 
     if(user){
       return res.status(400).json({error: "Username already exist"})
     }
+
+    if (user) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
@@ -20,19 +33,26 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      email,
+      password : hashedPassword,
       gender,
       profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
     })
 
-    await newUser.save();
+    if(newUser){ 
+        generateTokenAndSetCookie(newUser._id, res)
+        await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      userName: newUser.username,
-      profilePic: newUser.profilePic
+        res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          userName: newUser.username,
+          email: newUser.email,
+          profilePic: newUser.profilePic
     })
+    } else {
+      res.status(400).json({error: "Invalid user data"})
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message)
     res.status(500).json({error: "Internal server error"})
