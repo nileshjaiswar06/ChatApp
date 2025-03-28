c8cdca7072a48230a2c3f37a676a3cc9808b7750
Nilesh <nilesh.jaiswar006@gmail.com>
2025-03-28 19:41:52 +0530

    Signup endpoint

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,8 +1,41 @@
+import User from "../models/user.model.js"
+
 export const signup = async (req, res) => {
   try {
-    const {fullName, userName, password, confirmPassword, gender} = req.body
+    const {fullName, username, password, confirmPassword, gender} = req.body
+
+    if (password !== confirmPassword){
+      return res.status(400).json({error: "Passwords do not match"})
+    }
+
+    const user = await User.findOne({username})
+
+    if(user){
+      return res.status(400).json({error: "Username already exist"})
+    }
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+    })
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      userName: newUser.username,
+      profilePic: newUser.profilePic
+    })
   } catch (error) {
-    
+    console.log("Error in signup controller", error.message)
+    res.status(500).json({error: "Internal server error"})
   }
 }
 
--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,12 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js'
 
-const app = express()
-
 dotenv.config()
 
+const app = express()
 const PORT = process.env.PORT || 8000
 
-app.get('/', (req, res) => {
-  res.send("Hello Nilesh")
-})
+app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
