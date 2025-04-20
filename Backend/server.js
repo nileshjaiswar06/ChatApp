081e2cae44d176ccf75966bb1829aef9fe7387fe
Nilesh <nilesh.jaiswar006@gmail.com>
2025-04-20 18:38:30 +0530

    implemented  message route

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import conversationRoutes from './routes/conversation.routes.js'
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 8000
 console.log("first",process.env.PORT)
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
