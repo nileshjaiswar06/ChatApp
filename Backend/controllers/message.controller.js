517e804097ac0b9341a6f2904e61d36083509a6a
Nilesh <nilesh.jaiswar006@gmail.com>
2025-04-20 18:40:54 +0530

    created message controller so users can message each other and added a protective route to authorize users

---
+++ Backend/controllers/message.controller.js
@@ -0,0 +1,45 @@
+import Conversation from "../models/conversation.model.js"
+import Message from "../models/message.model.js"
+
+export const sendMessage = async (req, res) => {
+  try {
+    const {message} = req.body
+    const {id : receiverId} = req.params
+    const senderId = req.user._id
+
+    let conversation = await Conversation.findOne({
+      participants : {
+        $all : [senderId, receiverId]
+      }
+    })
+
+    if(!conversation) {
+      conversation = await Conversation.create({
+        participants : [senderId, receiverId],
+      })
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message
+    })
+
+    if(newMessage){
+      conversation.messages.push(newMessage._id)
+    }
+
+    // Save conversation first and then message
+    // await conversation.save() 
+    // await newMessage.save()
+
+    // Save both conversation and message in parallel
+    await Promise.all([conversation.save(), newMessage.save()])
+
+    res.status(201).json({newMessage})
+
+  } catch (error) {
+    console.log("error: ", error.message)
+    res.status(500).json({error : "Internal server error"})
+  }
+}
