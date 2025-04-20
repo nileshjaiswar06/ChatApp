0062eb8840e65ff21d8649927437958fb73365c9
Nilesh <nilesh.jaiswar006@gmail.com>
2025-04-20 17:28:44 +0530

    created conversation model (conversation schema )  and added message and conversation route

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -30,7 +30,10 @@ const userSchema = new mongoose.Schema({
     type : String,
     default : ""
   }
-})
+},
+  // createdAt, updatedAt
+  {timestamps : true}
+)
 
 const User = mongoose.model("User", userSchema)
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import conversationRoutes from './routes/conversation.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js'
 
 dotenv.config()
@@ -13,6 +15,8 @@ console.log("first",process.env.PORT)
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/conversations', conversationRoutes)
 
 app.listen(PORT, () => {
   connectToMongoDB();
