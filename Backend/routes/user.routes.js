210ad0f0ec5cbaeb6e3bd0fa498d1db834f4bcce
Nilesh <nilesh.jaiswar006@gmail.com>
2025-04-24 02:36:55 +0530

    added .js extension in routes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import protectRoute from '../middleware/protectRoute'
-import { getUsersForSidebar } from '../controllers/user.controller'
+import protectRoute from '../middleware/protectRoute.js'
+import { getUsersForSidebar } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
